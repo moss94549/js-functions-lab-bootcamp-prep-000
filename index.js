36cf5bec44eeb107a1b9205faa758f3b1bacb0c4
moss94549@gmail.com <moss94549@gmail.com>
2018-04-07 05:53:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,17 +6,15 @@ function happyHolidays()
 
 function happyHolidaysTo(name)
 {
-  return "Happy holidays, ${name}!";
+  return `Happy holidays, ${name}!`;
 }
 
-/*
 function happyHolidayTo(holiday, name)
 {
-  return 'Happy ${holiday}, ${name}!';
+  return `Happy ${holiday}, ${name}!`;
 }
 
 function holidayCountdown(holiday, numDays)
 {
-  return 'It\'s ${numDays} days until ${holiday}';
+  return `It\'s ${numDays} days until ${holiday}`;
 }
-*/
